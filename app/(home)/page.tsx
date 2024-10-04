70ea860ddf59b137a4f22a211120ba8363b60da6
miguel angel <162249501+orbitixsolutions@users.noreply.github.com>
2024-10-04 02:37:13 -0500

    FEAT: add starting now section

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,14 +1,16 @@
 import { Header } from './_components/header'
 import { Hero } from './_sections/hero'
 import { Registrations } from './_sections/registrations'
+import { StartingNow } from './_sections/starting-now'
 
 export default function SiteHomePage() {
   return (
     <>
       <Header />
-      <main className='max-w-7xl container mx-auto py-12 md:py-24 space-y-12 md:space-y-24 max-md:px-2'>
+      <main className='max-w-7xl container mx-auto py-12 md:py-24 space-y-16 md:space-y-32 max-md:px-2'>
         <Hero />
         <Registrations />
+        <StartingNow />
       </main>
     </>
   )

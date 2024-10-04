732cddf9485dde178498f58f2ee90a0eccd9d00f
miguel angel <162249501+orbitixsolutions@users.noreply.github.com>
2024-10-04 02:32:27 -0500

    FEAT: add registration section

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,5 +1,6 @@
 import { Header } from './_components/header'
 import { Hero } from './_sections/hero'
+import { Registrations } from './_sections/registrations'
 
 export default function SiteHomePage() {
   return (
@@ -7,6 +8,7 @@ export default function SiteHomePage() {
       <Header />
       <main className='max-w-7xl container mx-auto py-12 md:py-24 space-y-12 md:space-y-24 max-md:px-2'>
         <Hero />
+        <Registrations />
       </main>
     </>
   )

559260f48a9249bb7a84cfdf75215df5832f13dc
miguel angel <162249501+orbitixsolutions@users.noreply.github.com>
2024-10-09 22:13:00 -0500

    CHORE: add session provider

--- app/providers.tsx
+++ app/providers.tsx
@@ -2,6 +2,7 @@
 
 import * as React from 'react'
 import { ThemeProvider as NextThemesProvider } from 'next-themes'
+import { SessionProvider } from 'next-auth/react'
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
@@ -11,7 +12,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
       enableSystem
       disableTransitionOnChange
     >
-      {children}
+      <SessionProvider>{children}</SessionProvider>
     </NextThemesProvider>
   )
 }

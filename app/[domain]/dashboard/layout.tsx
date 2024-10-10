858142f331872782080494f48cc87214087964b5
miguel angel <162249501+orbitixsolutions@users.noreply.github.com>
2024-10-09 22:20:20 -0500

    FEAT: add dashboard page

--- app/[domain]/dashboard/layout.tsx
+++ app/[domain]/dashboard/layout.tsx
@@ -1,7 +1,21 @@
-export default function SiteLayout() {
+import { currentRole } from '@/data/auth'
+import { Header } from '@/app/[domain]/_shared/_components/header'
+import { redirect } from 'next/navigation'
+
+export default async function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  const ROLE = await currentRole()
+  if (ROLE === 'USER' || ROLE === 'STUDENT') return redirect('/')
+
   return (
-    <div>
-      <h2>Site Layout</h2>
-    </div>
+    <>
+      <Header />
+      <main className='space-y-20 md:space-y-12 py-20 md:p-12 container mx-auto'>
+        {children}
+      </main>
+    </>
   )
 }
--- app/[domain]/dashboard/page.tsx
+++ app/[domain]/dashboard/page.tsx
@@ -1,7 +1,33 @@
-export default function SiteHomePage() {
+import { ecaExists } from '@/services/helpers/eca-exists'
+import { capitalizeLetters } from '@/services/utils/uppercase-strings'
+import { redirect } from 'next/navigation'
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { domain: string }
+}) {
+  const ECA_NAME = capitalizeLetters(params.domain.split('-').join(' '))
+
+  return {
+    title: `Dashboard - ${ECA_NAME}`,
+  }
+}
+
+export default function SitePageDashboard({
+  params,
+}: {
+  params: { domain: string }
+}) {
+  const DOMAIN = decodeURIComponent(params.domain)
+  const ECA_NAME = capitalizeLetters(DOMAIN.split('-').join(' '))
+
+  const ECA_EXISTS = ecaExists(DOMAIN)
+  if (!ECA_EXISTS) return redirect('/')
+
   return (
-    <div>
-      <h2>Home Page</h2>
-    </div>
+    <section className='space-y-8'>
+      <h2>Bienvenido al panel de administración de {ECA_NAME}</h2>
+    </section>
   )
 }

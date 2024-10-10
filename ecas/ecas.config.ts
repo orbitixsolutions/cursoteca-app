cc3a7a655faa821aa362868f6a9576a6145ed124
miguel angel <162249501+orbitixsolutions@users.noreply.github.com>
2024-10-09 22:09:33 -0500

    FEAT: add ecas config file

---
+++ ecas/ecas.config.ts
@@ -0,0 +1,21 @@
+import { LosPinosLogo } from '@/assets/ecas/logos'
+import { HeroImg } from '@/assets/ecas/images'
+
+export const ECAS = [
+  {
+    name: 'Los Pinos',
+    id: 'los-pinos',
+    description:
+      'Los Pinos es un programa de educación que busca darle a los estudiantes la oportunidad de aprender de manera gratuita y en un ambiente de calidad.',
+    logo: LosPinosLogo.src,
+    image: HeroImg.src,
+  },
+  {
+    name: 'Test',
+    id: 'test',
+    description:
+      'Los Test es un programa de educación que busca darle a los estudiantes la oportunidad de aprender de manera gratuita y en un ambiente de calidad.',
+    logo: LosPinosLogo.src,
+    image: HeroImg.src,
+  },
+]

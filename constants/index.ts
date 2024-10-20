2e870d6800507bc805e71928654cfd06902c26c9
miguel angel <162249501+orbitixsolutions@users.noreply.github.com>
2024-10-20 07:01:22 -0500

    CHORE: add course categories

--- constants/index.ts
+++ constants/index.ts
@@ -77,3 +77,26 @@ export const SIDEBAR_ITEMS = [
     icon: Settings,
   },
 ]
+
+export const COURSE_CATEGORIES = [
+  {
+    name: 'none',
+    value: 'NONE',
+    label: 'Ninguno',
+  },
+  {
+    name: 'logistics',
+    value: 'LOGISTICS',
+    label: 'Logistica',
+  },
+  {
+    name: 'pharmaceuticals',
+    value: 'PHARMACEUTICALS',
+    label: 'Farmaceútica',
+  },
+  {
+    name: 'others',
+    value: 'OTHERS',
+    label: 'Otros',
+  },
+]

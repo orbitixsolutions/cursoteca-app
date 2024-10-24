ea55b2d108dafaacf25db2cc504c8b74b72fd4e9
miguel angel <162249501+orbitixsolutions@users.noreply.github.com>
2024-10-23 23:31:04 -0500

    STYLE: update padding button

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -25,7 +25,7 @@ const buttonVariants = cva(
         sm: "h-8 rounded-md px-3 text-xs",
         lg: "h-10 rounded-md px-8",
         icon: "h-9 w-9",
-        full: "w-full h-9",
+        full: "w-full h-9 px-4 py-2",
       },
     },
     defaultVariants: {

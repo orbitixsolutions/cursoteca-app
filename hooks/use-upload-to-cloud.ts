96d4ff184dae155a4caea258af56d7b5ccdaab3e
miguel angel <162249501+orbitixsolutions@users.noreply.github.com>
2024-10-23 23:33:21 -0500

    FEAT: add cloudinary image functions

---
+++ hooks/use-upload-to-cloud.ts
@@ -0,0 +1,48 @@
+import { useRouter } from 'next/navigation'
+import { useStore } from 'zustand'
+import { useFileImage } from '@/services/store/use-file-image'
+import { useCurrentRole } from './use-session'
+import { cloudinaryUpload } from '@/services/utils/cloudinary-upload'
+import { toast } from 'sonner'
+
+type HandleUploadImageProps = {
+  id: string | undefined
+  folder: string
+  path: string
+}
+
+export function useUploadImageToCloud() {
+  const { refresh } = useRouter()
+  const ROLE = useCurrentRole()
+
+  const FILE_STORE = useStore(useFileImage, (state) => state)
+
+  const handleUpload = async ({ id, folder, path }: HandleUploadImageProps) => {
+    const IMAGE_FILE = FILE_STORE?.imageFile
+
+    if (!IMAGE_FILE) return
+    if (ROLE === 'USER' || ROLE === 'STUDENT') {
+      return toast.error('No tienes permisos.')
+    }
+
+    const formData = new FormData()
+    formData.append(`${path}-files`, IMAGE_FILE as File)
+
+    const { status } = await cloudinaryUpload({
+      folder,
+      form: formData,
+      itemId: id,
+      path,
+    })
+
+    if (status === 201) {
+      refresh()
+      FILE_STORE.onClear()
+      return
+    }
+
+    FILE_STORE.onClear()
+  }
+
+  return { handleUpload }
+}

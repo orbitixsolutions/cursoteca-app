b8ad7f68d499e01b432ffac918097c0dba295e77
miguel angel <aroque@orbitixsolutions.com>
2024-11-03 11:47:18 -0500

    REFACTOR: cursoteca index page

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,17 +1,7 @@
-import { Header } from './_components/header'
-import { Hero } from './_sections/hero'
-import { Registrations } from './_sections/registrations'
-import { StartingNow } from './_sections/starting-now'
-
-export default function SiteHomePage() {
+export default function HomePage() {
   return (
-    <>
-      <Header />
-      <main className='max-w-7xl container mx-auto py-12 md:py-24 space-y-16 md:space-y-32 max-md:px-2'>
-        <Hero />
-        <Registrations />
-        <StartingNow />
-      </main>
-    </>
+    <main className='max-w-7xl container mx-auto py-12 md:py-24 space-y-16 md:space-y-32 max-md:px-2'>
+      Pagina Principal Cursoteca
+    </main>
   )
 }

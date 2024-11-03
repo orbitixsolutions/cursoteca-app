f1ce7476a101da24497d5118096d473bf74fe419
miguel angel <aroque@orbitixsolutions.com>
2024-11-03 11:50:32 -0500

    FEAT: add course data by id fetching function

---
+++ app/[domain]/(courses)/course/[id]/_services/fetch.ts
@@ -0,0 +1,10 @@
+import db from '@/lib/db'
+
+export async function getCourseById(id: string) {
+  try {
+    const COURSE = await db.course.findUnique({ where: { id } })
+    return COURSE
+  } catch (error) {
+    return null
+  }
+}

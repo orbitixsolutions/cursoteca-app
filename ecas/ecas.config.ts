1c06353a6f2b15598831335198b5a9934f33db52
miguel angel <aroque@orbitixsolutions.com>
2024-11-03 12:03:05 -0500

    CHORE: add eca config props

--- ecas/ecas.config.ts
+++ ecas/ecas.config.ts
@@ -1,6 +1,15 @@
 import { LosPinosLogo } from '@/assets/ecas/logos'
 import { HeroImg } from '@/assets/ecas/images'
 
+export type EcaProps = {
+  name: string
+  id: string
+  description: string
+  logo: string
+  image: string
+  color: string
+}
+
 export const ECAS = [
   {
     name: 'Los Pinos',
@@ -9,6 +18,7 @@ export const ECAS = [
       'Los Pinos es un programa de educación que busca darle a los estudiantes la oportunidad de aprender de manera gratuita y en un ambiente de calidad.',
     logo: LosPinosLogo.src,
     image: HeroImg.src,
+    color: '',
   },
   {
     name: 'Test',
@@ -17,5 +27,6 @@ export const ECAS = [
       'Los Test es un programa de educación que busca darle a los estudiantes la oportunidad de aprender de manera gratuita y en un ambiente de calidad.',
     logo: LosPinosLogo.src,
     image: HeroImg.src,
+    color: '',
   },
 ]

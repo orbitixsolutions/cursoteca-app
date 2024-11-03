3ed59cc25891c2cb6d9d0b058a28341b33d844fc
miguel angel <aroque@orbitixsolutions.com>
2024-11-03 12:11:27 -0500

    CHORE: update params props

--- services/helpers/eca-exists.ts
+++ services/helpers/eca-exists.ts
@@ -1,6 +1,6 @@
 import { ECAS } from "@/ecas/ecas.config"
 
-export function ecaExists(domain: string) {
+export function ecaExists(domain: string | undefined) {
   const ECA_EXISTS = ECAS.some((eca) => eca.id === domain)
   return ECA_EXISTS
 }

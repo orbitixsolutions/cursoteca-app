d2cc10362b8b59c4b04e7b7b8424d741245321c3
miguel angel <aroque@orbitixsolutions.com>
2024-11-03 12:11:42 -0500

    REFACTOR: get eca name function

--- services/helpers/get-eca-name.ts
+++ services/helpers/get-eca-name.ts
@@ -1,3 +1,10 @@
-export function getEcaName(name: string) {
-  return name.replaceAll('-', ' ')
+export function getEcaName(domain: string) {
+  const DOMAIN = decodeURIComponent(domain)
+  const ECA_NAME = capitalizeLetters(DOMAIN.split('-').join(' '))
+
+  return { DOMAIN, ECA_NAME }
+}
+
+export function capitalizeLetters(string: string) {
+  return string.replace(/\b\w/g, (char) => char.toUpperCase())
 }

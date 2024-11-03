f6c751d63e855c894d2c07e3f68158c10c004ab4
miguel angel <aroque@orbitixsolutions.com>
2024-11-03 12:04:44 -0500

    CHORE: add hover card course preview

--- app/[domain]/dashboard/courses/_components/course-table/course.column.tsx
+++ app/[domain]/dashboard/courses/_components/course-table/course.column.tsx
@@ -1,5 +1,10 @@
 'use client'
 
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from '@/components/ui/hover-card'
 import { Course } from '@prisma/client'
 import { ColumnDef } from '@tanstack/react-table'
 import { AspectRatio } from '@/components/ui/aspect-ratio'
@@ -8,29 +13,51 @@ import { Delete } from '@/app/[domain]/dashboard/courses/_components/actions/del
 import { Edit } from '@/app/[domain]/dashboard/courses/_components/actions/edit'
 import { Badge } from '@/components/ui/badge'
 import { getCategoryName } from '@/services/utils/get-category-name'
-import Image from 'next/image' 
+import { CardTitle } from '@/components/ui/card'
+import Image from 'next/image'
 
 export const courseColumns: ColumnDef<Course>[] = [
   {
     accessorKey: 'image',
     header: 'Imagen',
     cell: ({ row }) => {
-      const { imageUrl } = row.original
+      const { imageUrl, title, description } = row.original
       const IMAGE_EXITS =
         imageUrl === 'NO_IMAGE' ? CoursePlaceholderImg.src : imageUrl
 
       return (
-        <div className='size-24'>
-          <AspectRatio ratio={1 / 1}>
-            <Image
-              src={IMAGE_EXITS}
-              width={720}
-              height={720}
-              alt='Imagen del curso'
-              className='rounded-md object-cover'
-            />
-          </AspectRatio>
-        </div>
+        <HoverCard>
+          <HoverCardTrigger asChild>
+            <div className='size-24'>
+              <AspectRatio ratio={1 / 1}>
+                <Image
+                  src={IMAGE_EXITS}
+                  width={720}
+                  height={720}
+                  alt='Imagen del curso'
+                  className='rounded-md object-cover'
+                />
+              </AspectRatio>
+            </div>
+          </HoverCardTrigger>
+          <HoverCardContent className='w-80'>
+            <div className='space-y-4'>
+              <AspectRatio ratio={16 / 9}>
+                <Image
+                  src={imageUrl}
+                  alt={`Curso: ${title}`}
+                  width={1600}
+                  height={900}
+                  className='size-full object-cover rounded-lg'
+                />
+              </AspectRatio>
+              <article className='space-y-2'>
+                <CardTitle>{title}</CardTitle>
+                <p className='text-sm opacity-60'>{description}</p>
+              </article>
+            </div>
+          </HoverCardContent>
+        </HoverCard>
       )
     },
   },
@@ -62,7 +89,7 @@ export const courseColumns: ColumnDef<Course>[] = [
   },
   {
     accessorKey: 'actions',
-    header: '',
+    header: 'Acciones',
     cell: ({ row }) => {
       const { id } = row.original
 

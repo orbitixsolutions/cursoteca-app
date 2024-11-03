21ac093479fff36cb992d85fee5f9925e8402880
miguel angel <aroque@orbitixsolutions.com>
2024-11-03 11:45:59 -0500

    CHORE: remove missing util function

--- services/utils/uppercase-strings.ts
+++
@@ -1,7 +0,0 @@
-export function uppercaseFirstLetter(string: string) {
-  return string.charAt(0).toUpperCase() + string.slice(1)
-}
-
-export function capitalizeLetters(string: string) {
-  return string.replace(/-/g, ' ').replace(/\b\w/g, (l) => l.toUpperCase())
-}

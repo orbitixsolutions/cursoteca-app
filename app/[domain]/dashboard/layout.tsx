5ba3c94445995970063ffba7a980d2e33ac88177
miguel angel <aroque@orbitixsolutions.com>
2024-11-03 12:10:17 -0500

    REFACTOR: dashboard layout

--- app/[domain]/dashboard/layout.tsx
+++ app/[domain]/dashboard/layout.tsx
@@ -8,6 +8,9 @@ import { redirect } from 'next/navigation'
 import { Separator } from '@/components/ui/separator'
 import { DashboardSidebar } from '@/app/[domain]/dashboard/_components/dashboard-sidebar'
 import { DashboardBreadcrumbs } from '@/app/[domain]/dashboard/_components/dashboard-breadcrumbs'
+import { EcaProvider } from '@/app/[domain]/provider'
+import { ECAS } from '@/ecas/ecas.config'
+import { getEcaName } from '@/services/helpers/get-eca-name'
 
 export default async function DashboardLayout({
   children,
@@ -16,33 +19,37 @@ export default async function DashboardLayout({
   children: React.ReactNode
   params: { domain: string }
 }) {
+  const { DOMAIN } = getEcaName(domain)
+
   const USER = await currentUser()
+  const ECA_CONFIG = ECAS.find((e) => e.id === DOMAIN)
 
-  const USER_ECA = USER?.ecaId
   const ROLE = USER?.role
-
-  if (USER_ECA === undefined) return redirect('/')
-
-  if (USER_ECA !== domain) return redirect(`/${USER_ECA}/dashboard`)
 
   if (ROLE === 'USER' || ROLE === 'STUDENT') return redirect('/')
 
   return (
-    <SidebarProvider>
-      <DashboardSidebar />
-      <SidebarInset>
-        <header className='flex h-16 shrink-0 items-center gap-2 border-b px-4'>
-          <SidebarTrigger className='-ml-1' />
-          <Separator
-            orientation='vertical'
-            className='mr-2 h-4'
-          />
-          <DashboardBreadcrumbs />
-        </header>
-        <main className='flex flex-1 flex-col gap-4 p-8 container mx-auto'>
-          {children}
-        </main>
-      </SidebarInset>
-    </SidebarProvider>
+    <EcaProvider
+      user={USER}
+      config={ECA_CONFIG}
+      isAuthRequired
+    >
+      <SidebarProvider>
+        <DashboardSidebar />
+        <SidebarInset>
+          <header className='flex h-16 shrink-0 items-center gap-2 border-b px-4'>
+            <SidebarTrigger className='-ml-1' />
+            <Separator
+              orientation='vertical'
+              className='mr-2 h-4'
+            />
+            <DashboardBreadcrumbs />
+          </header>
+          <main className='flex flex-1 flex-col gap-4 p-8 container mx-auto'>
+            {children}
+          </main>
+        </SidebarInset>
+      </SidebarProvider>
+    </EcaProvider>
   )
 }

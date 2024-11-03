9ef15fcb1a08b514ce44c69017a48bf14baa60c3
miguel angel <aroque@orbitixsolutions.com>
2024-11-03 12:06:19 -0500

    REFACTOR: auth page

--- app/[domain]/auth/page.tsx
+++ app/[domain]/auth/page.tsx
@@ -1,35 +1,36 @@
-import { capitalizeLetters } from '@/services/utils/uppercase-strings'
 import { LoginForm } from '@/app/[domain]/auth/_components/login-form'
 import { redirect } from 'next/navigation'
 import { ecaExists } from '@/services/helpers/eca-exists'
+import { getEcaName } from '@/services/helpers/get-eca-name'
 
 export async function generateMetadata({
-  params,
+  params: { domain },
 }: {
   params: { domain: string }
 }) {
-  const ECA_NAME = capitalizeLetters(params.domain.split('-').join(' '))
+  const { ECA_NAME } = getEcaName(domain)
 
   return {
     title: `Acceder - ${ECA_NAME}`,
   }
 }
 
-export default function SiteAuthPage({
-  params,
+export default function AuthPage({
+  params: { domain },
 }: {
   params: { domain: string }
 }) {
-  const DOMAIN = decodeURIComponent(params.domain)
-  const ECA_NAME = capitalizeLetters(DOMAIN.split('-').join(' '))
+  const { DOMAIN, ECA_NAME } = getEcaName(domain)
 
   const ECA_EXITS = ecaExists(DOMAIN)
   if (!ECA_EXITS) return redirect('/')
 
   return (
-    <section className='flex flex-col items-center justify-center h-screen'>
-      <h2 className='text-center text-xl font-bold'>Acceder - {ECA_NAME}</h2>
-      <LoginForm />
-    </section>
+    <main className='size-full min-h-screen'>
+      <section className='flex flex-col items-center justify-center h-screen'>
+        <h2 className='text-center text-xl font-bold'>Acceder - {ECA_NAME}</h2>
+        <LoginForm />
+      </section>
+    </main>
   )
 }

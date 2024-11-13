e1b3f8514de897fd88a67c882a497ed90ca9b2fc
miguel angel <162249501+orbitixsolutions@users.noreply.github.com>
2024-11-13 16:08:35 -0500

    CHORE: add new button variant

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -24,6 +24,7 @@ const buttonVariants = cva(
         default: "h-9 px-4 py-2",
         sm: "h-8 rounded-md px-3 text-xs",
         lg: "h-10 rounded-md px-8",
+        full: "w-full h-9 rounded-md px-4 py-2",
         icon: "h-9 w-9",
       },
     },

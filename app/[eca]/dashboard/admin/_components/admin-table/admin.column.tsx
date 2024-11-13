a669ef110e0115970013f7aebf719fbd5f5fbcf6
miguel angel <162249501+orbitixsolutions@users.noreply.github.com>
2024-11-13 04:07:54 -0500

    FEAT: add table column

---
+++ app/[eca]/dashboard/admin/_components/admin-table/admin.column.tsx
@@ -0,0 +1,47 @@
+'use client'
+
+import { Badge } from '@/components/ui/badge'
+import { getRole } from '@/helpers/get-role'
+import { User } from '@prisma/client'
+import { ColumnDef } from '@tanstack/react-table'
+import { AdminActions } from '@/app/[eca]/dashboard/admin/_components/admin-actions'
+import { CopyField } from '../copy-field'
+
+export type Admins = User
+
+export const AdminColumns: ColumnDef<Admins>[] = [
+  {
+    accessorKey: 'name',
+    header: 'Nombre',
+    cell: ({ row }) => {
+      const { name } = row.original
+      return name
+    },
+  },
+  {
+    accessorKey: 'email',
+    header: 'Correo electrónico',
+    cell: ({ row }) => {
+      const { email } = row.original
+      return <CopyField label={email} />
+    },
+  },
+  {
+    accessorKey: 'role',
+    header: 'Rol',
+    cell: ({ row }) => {
+      const { role } = row.original
+      const ROLE = getRole(role)
+
+      return <Badge>{ROLE}</Badge>
+    },
+  },
+  {
+    accessorKey: 'actions',
+    header: 'Acciones',
+    cell: ({ row }) => {
+      const { id } = row.original
+      return <AdminActions id={id} />
+    },
+  },
+]

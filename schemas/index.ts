37aeffc714a891b44266cc73b975bdd4198abfce
miguel angel <162249501+orbitixsolutions@users.noreply.github.com>
2024-11-13 03:56:16 -0500

    CHORE: add create admin zod schema

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,6 +1,26 @@
 import * as z from 'zod'
 
 export const LoginSchema = z.object({
-  email: z.string().email(),
-  password: z.string(),
+  email: z.string().email({
+    message: 'Por favor ingrese un email válido.',
+  }),
+  password: z.string().min(6, {
+    message: 'La contraseña debe tener al menos 6 caracteres.',
+  }),
+})
+
+export const CreateAdminSchema = z.object({
+  name: z.string().min(3, {
+    message: 'El nombre debe tener al menos 3 caracteres.',
+  }),
+  email: z.string().email({
+    message: 'Por favor ingrese un email válido.',
+  }),
+  password: z.string().min(6, {
+    message: 'La contraseña debe tener al menos 6 caracteres.',
+  }),
+  eca: z.string().min(1, {
+    message: 'La ECA es obligatoria.',
+  }),
+  role: z.enum(['ADMIN', 'DIRECTIVE', 'USER']),
 })

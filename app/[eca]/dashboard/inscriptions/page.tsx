d3d1327287b721f73df76c33eefe211d3f8e0fb0
miguel angel <162249501+orbitixsolutions@users.noreply.github.com>
2024-11-14 18:21:46 -0500

    FEAT: add inscriptions page

---
+++ app/[eca]/dashboard/inscriptions/page.tsx
@@ -0,0 +1,36 @@
+import { ContentLayout } from '@/components/shared/dashboard/content-layout'
+import { getEcaName } from '@/helpers/get-eca-name'
+import { getInscriptions } from '@/app/[eca]/dashboard/inscriptions/_services/fetch'
+import { DataTable } from '@/components/data-table'
+import { InscriptionColumns } from '@/app/[eca]/dashboard/inscriptions/_components/inscription-table/inscription-column'
+
+interface InscriptionsPageProps {
+  params: {
+    eca: string
+  }
+}
+
+export default async function InscriptionsPage(props: InscriptionsPageProps) {
+  const { params } = props
+  const { DOMAIN } = getEcaName(params.eca)
+
+  const INSCRIPTIONS = await getInscriptions(DOMAIN)
+
+  return (
+    <ContentLayout title='Inscripciones'>
+      <div className='space-y-5'>
+        <div className='space-y-4 flex items-center justify-between'>
+          <article>
+            <h1 className='text-2xl font-bold'>Inscriptos</h1>
+            <p>Gestiona las inscriptos en este panel.</p>
+          </article>
+        </div>
+
+        <DataTable
+          data={INSCRIPTIONS ?? []}
+          columns={InscriptionColumns}
+        />
+      </div>
+    </ContentLayout>
+  )
+}

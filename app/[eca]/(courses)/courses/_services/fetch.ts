f1de651883f113fdc3fc655d3d11b4594740b660
miguel angel <162249501+orbitixsolutions@users.noreply.github.com>
2024-11-13 22:18:49 -0500

    FEAT: add courses action

---
+++ app/[eca]/(courses)/courses/_services/fetch.ts
@@ -0,0 +1,21 @@
+import { groupedCourses } from '@/utils/grouped-courses'
+import db from '@/lib/db'
+
+export async function getCourses(eca: string) {
+  try {
+    const COURSES = await db.course.findMany({
+      where: {
+        ecaId: eca,
+        isVisible: true,
+      },
+      orderBy: {
+        createdAt: 'asc',
+      },
+    })
+
+    const GROUPED_COURSES = groupedCourses(COURSES)
+    return GROUPED_COURSES
+  } catch (error) {
+    return null
+  }
+}

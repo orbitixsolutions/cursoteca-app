2189a9cfc7315adaf2988079f271f66abe1e8e74
miguel angel <162249501+orbitixsolutions@users.noreply.github.com>
2024-11-13 21:00:39 -0500

    CHORE: add types to eca config

--- lib/ecas.config.ts
+++ lib/ecas.config.ts
@@ -1,9 +1,22 @@
-export const ECAS_CONFIG = [
+import { LosPinosHeroImg } from '@/assets/ecas/images'
+import { LosPinosLogo } from '@/assets/ecas/logos'
+
+export type EcaConfigProps = {
+  name: string
+  description: string
+  id: string
+  image: string
+  logo: string
+  path: string
+}
+
+export const ECAS_CONFIG: EcaConfigProps[] = [
   {
     name: 'Los pinos',
     description: 'Institucion de todos y para todos.',
     id: 'los-pinos',
-    image: '',
-    url: '',
+    image: LosPinosHeroImg.src,
+    logo: LosPinosLogo.src,
+    path: 'los-pinos',
   },
 ]

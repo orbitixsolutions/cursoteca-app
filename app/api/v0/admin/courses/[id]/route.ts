7a9129260430ab6ada52e457c128178490796eed
miguel angel <162249501+orbitixsolutions@users.noreply.github.com>
2024-11-14 08:27:26 -0500

    FEAT: minor changes

--- app/api/v0/admin/courses/[id]/route.ts
+++ app/api/v0/admin/courses/[id]/route.ts
@@ -1,4 +1,5 @@
 import { NextResponse } from 'next/server'
+import { currentRole } from '@/lib/session'
 import db from '@/lib/db'
 
 export const dynamic = 'force-dynamic'
@@ -7,6 +8,11 @@ export async function GET(
   request: Request,
   { params: { id } }: { params: { id: string } }
 ) {
+  const ROLE = await currentRole()
+
+  if (ROLE === 'USER' || !ROLE)
+    return NextResponse.json({ error: 'No tienes permisos.' }, { status: 401 })
+
   try {
     const COURSE = await db.course.findUnique({
       where: {
@@ -15,7 +21,7 @@ export async function GET(
     })
 
     return NextResponse.json(COURSE, { status: 201 })
-  } catch (error) {
-    return NextResponse.json({ error }, { status: 500 })
+  } catch {
+    return NextResponse.json(null, { status: 500 })
   }
 }

37e7996a8cd8fbd0ba02d03e44b236fe58e51688
miguel angel <162249501+orbitixsolutions@users.noreply.github.com>
2024-11-14 00:20:36 -0500

    FEAT: add const for selects

--- constants/index.ts
+++ constants/index.ts
@@ -1,1 +1,41 @@
+export const EDUCATIONAL_LEVELS = [
+  {
+    label: 'Ninguno',
+    value: 'NONE',
+  },
+  {
+    label: 'Primaria',
+    value: 'PRIMARY',
+  },
+  {
+    label: 'Ciclo Básico',
+    value: 'BASIC_CYCLE',
+  },
+  {
+    label: 'Secundaria',
+    value: 'SECONDARY',
+  },
+  {
+    label: 'Universitaria',
+    value: 'UNIVERSITY',
+  },
+]
 
+export const SELECT_COURSE_CATEGORIES = [
+  {
+    value: 'NONE',
+    label: 'Ninguno',
+  },
+  {
+    value: 'LOGISTICS',
+    label: 'Logistica',
+  },
+  {
+    value: 'PHARMACEUTICALS',
+    label: 'Farmaceútica',
+  },
+  {
+    value: 'OTHERS',
+    label: 'Otros',
+  },
+]

1c8b7d75ae4bffdf9e57dcaa254719313d8ac4c5
miguel angel <162249501+orbitixsolutions@users.noreply.github.com>
2024-11-13 22:18:32 -0500

    FEAT: add course layout

---
+++ app/[eca]/(courses)/layout.tsx
@@ -0,0 +1,38 @@
+import { EcaFooter } from '@/components/shared/general/eca-footer'
+import { EcaHeader } from '@/components/shared/general/eca-header'
+import { getEcaData } from '@/helpers/get-eca-data'
+import { getEcaName } from '@/helpers/get-eca-name'
+import { EcaProvider } from '@/app/[eca]/provider'
+import { redirect } from 'next/navigation'
+import { getCourses } from '@/app/[eca]/(courses)/courses/_services/fetch'
+import { GroupedCoursesProps } from '@/utils/grouped-courses'
+
+export default async function CourseLayout({
+  children,
+  params: { eca },
+}: {
+  children: React.ReactNode
+  params: { eca: string }
+}) {
+  const { DOMAIN } = getEcaName(eca)
+
+  const ECA_DATA = getEcaData(DOMAIN)
+  const ECA_COURSES = (await getCourses(eca)) as Array<GroupedCoursesProps>
+
+  if (!ECA_DATA) return redirect('/')
+
+  return (
+    <EcaProvider
+      data={ECA_DATA}
+      courses={ECA_COURSES}
+    >
+      <EcaHeader data={ECA_DATA} />
+
+      <main className='max-w-[1280px] w-full mx-auto py-20 px-8'>
+        {children}
+      </main>
+
+      <EcaFooter data={ECA_DATA} />
+    </EcaProvider>
+  )
+}

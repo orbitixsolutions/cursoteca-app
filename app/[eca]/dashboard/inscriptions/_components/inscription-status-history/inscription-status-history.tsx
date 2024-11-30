dff6e4aad9b080f9ca66e973712abda0b6f6fa77
miguel angel <162249501+orbitixsolutions@users.noreply.github.com>
2024-11-29 20:26:29 -0500

    CHORE: add delete button

--- app/[eca]/dashboard/inscriptions/_components/inscription-status-history/inscription-status-history.tsx
+++ app/[eca]/dashboard/inscriptions/_components/inscription-status-history/inscription-status-history.tsx
@@ -4,13 +4,38 @@ import {
   PopoverTrigger,
 } from '@/components/ui/popover'
 import { Button } from '@/components/ui/button'
-import { Dot, History } from 'lucide-react'
+import { Dot, History, Trash2 } from 'lucide-react'
 import { InscriptionStatusHistoryProps } from '@/app/[eca]/dashboard/inscriptions/_components/inscription-status-history/inscription-status-history.type'
 import { getClassByStatus, getNameByStatus } from '@/helpers/get-state'
+import { Card } from '@/components/ui/card'
+import { DeleteButton } from '@/components/delete-button'
+import { deleteInscriptionStatus } from '@/app/[eca]/dashboard/inscriptions/_services/delete'
 import { cn } from '@/lib/utils'
 
 export function InscriptionStatusHistory(props: InscriptionStatusHistoryProps) {
   const { status } = props
+
+  const STATUS = status.map((state) => (
+    <li key={state.id}>
+      <Card className='flex items-center justify-between space-x-2 p-4'>
+        <div className='flex items-center'>
+          <Dot className={cn(getClassByStatus(state.status))} />
+          <h3 className='text-sm'>{getNameByStatus(state.status)}</h3>
+        </div>
+        
+        <div className='flex items-center gap-3'>
+          <p>{new Date(state.createdAt).toLocaleDateString()}</p>
+
+          <DeleteButton
+            itemId={state.id}
+            onDelete={deleteInscriptionStatus}
+          >
+            <Trash2 />
+          </DeleteButton>
+        </div>
+      </Card>
+    </li>
+  ))
 
   return (
     <Popover>
@@ -20,27 +45,9 @@ export function InscriptionStatusHistory(props: InscriptionStatusHistoryProps) {
         </Button>
       </PopoverTrigger>
       <PopoverContent className='w-96'>
-        <div>
-          <ul className='grid gap-4'>
-            {status.length > 0 ? (
-              status.map((state) => (
-                <li key={state.id}>
-                  <div className='flex items-center justify-between space-x-2'>
-                    <div className='flex items-center'>
-                      <Dot className={cn(getClassByStatus(state.status))} />
-                      <h3 className='text-sm'>
-                        {getNameByStatus(state.status)}
-                      </h3>
-                    </div>
-                    <p>{new Date(state.createdAt).toLocaleDateString()}</p>
-                  </div>
-                </li>
-              ))
-            ) : (
-              <p className='text-sm text-center'>Sin historial</p>
-            )}
-          </ul>
-        </div>
+        <ul className='space-y-2'>
+          {!STATUS.length ? <p>No hay historial</p> : STATUS}
+        </ul>
       </PopoverContent>
     </Popover>
   )

e3f3dc556aab91f9e3c418d81b8e6ba3b45a4fac
miguel angel <162249501+orbitixsolutions@users.noreply.github.com>
2024-11-29 20:24:54 -0500

    CHORE: add create inscription action

--- app/[eca]/dashboard/inscriptions/_services/create.ts
+++ app/[eca]/dashboard/inscriptions/_services/create.ts
@@ -3,6 +3,8 @@
 import { currentRole } from '@/lib/session'
 import { STATUS_ENUM } from '@prisma/client'
 import db from '@/lib/db'
+import { z } from 'zod'
+import { InscriptionComentSchema } from '@/schemas'
 
 export async function createInscriptionStatus(
   status: STATUS_ENUM | undefined,
@@ -23,6 +25,38 @@ export async function createInscriptionStatus(
     })
 
     return { status: 201, message: 'Estado agregado.' }
+  } catch {
+    return { status: 500, message: 'Ha ocurrido un error.' }
+  }
+}
+
+export async function createInscriptionComment(
+  data: z.infer<typeof InscriptionComentSchema>,
+  enrollmentId: string
+) {
+  const ROLE = await currentRole()
+
+  if (ROLE === 'ENROLLED') {
+    return { status: 400, message: 'No tienes permisos' }
+  }
+
+  const VALIDATION = InscriptionComentSchema.safeParse(data)
+
+  if (!VALIDATION.success) {
+    return { status: 400, message: 'Campos invalidos.' }
+  }
+
+  const { comment } = VALIDATION.data
+
+  try {
+    await db.enrollmentComment.create({
+      data: {
+        enrollmentId,
+        comment,
+      },
+    })
+
+    return { status: 201, message: 'Comentario agregado.' }
   } catch {
     return { status: 500, message: 'Ha ocurrido un error.' }
   }

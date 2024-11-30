06a10af59e6649a763cc403de3f41a2848c3fdb4
miguel angel <162249501+orbitixsolutions@users.noreply.github.com>
2024-11-29 20:26:13 -0500

    CHORE: add redirect (/los-pinos)

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,9 +1,5 @@
+import { redirect } from "next/navigation";
+
 export default function Home() {
-  return (
-    <main className='flex min-h-screen flex-col items-center justify-center py-2'>
-      <h2 className='text-7xl font-bold tracking-tight text-center text-primary-foreground sm:text-4xl'>
-        Cursoteca Home
-      </h2>
-    </main>
-  )
+  return redirect('/los-pinos')
 }
